74fb353cf9f85ed5ac99b4be009bf04d31ba6206
rajeev kumar <rajeev.code@gmail.com>
2019-05-05 15:23:36 +0800

    dictionaries

---
+++ django_lectures/dictionaries.py
@@ -0,0 +1,17 @@
+# Dictionaries
+#Dictionaries do not reatain any sort of orders
+my_stuff = {"key1":"value1","key2":"value2",'key3':{'123':[1,2,'grabMe']}}
+print(my_stuff)
+print(my_stuff['key1'])
+print(my_stuff ['key3']['123'][2]).upper()
+
+print(my_stuff ['key3']['123'][2]).lower()
+
+# Dictionaries key value1
+
+my_meals = {'lunch':'pizza','bfast':'eggs'}
+my_meals['lunch'] = 'burger'
+my_meals['dinner'] = 'pasta'
+print(my_meals['lunch'])
+print(my_meals['dinner'])
+print(my_meals)

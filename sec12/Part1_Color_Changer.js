0c2d4104b912e109a929831e22a898a610f02363
rajeev kumar <rajeev.code@gmail.com>
2019-09-23 16:00:30 +0800

    commit DOM code

---
+++ sec12/Part1_Color_Changer.js
@@ -0,0 +1,17 @@
+  function getRandomColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  function setRandomColor() {
+  $("#colorpad").css("background-color", getRandomColor());
+}
+  // function changeHeaderColor(){
+  //   colorInput = getRandomColor()
+  //   header.style.color = colorInput;
+  // }
+  // setInterval("changeHeaderColor()",100);
